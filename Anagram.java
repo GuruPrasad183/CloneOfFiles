098d76225b6d0d995b1e5e228cac3986568a0af9
Temporary183 <halej36450@alibrs.com>
2024-01-28 18:31:00 +0530

    modified from primary

--- Anagram.java
+++ Anagram.java
@@ -6,9 +6,9 @@ class Anagram
 	{
 
 	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter the first String that is from the temporary account:");
+	System.out.println("Enter the first String that is from the primary account:");
 	String s1=sc.next();
-	System.out.println("Enter the first String that is from the temporary account:");
+	System.out.println("Enter the first String that is from the primary account:");
 	String s2=sc.next();
 	if(s1.length()==s2.length())
 	{
